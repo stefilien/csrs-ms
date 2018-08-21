6361083f92d1106b0985ca7de78101b8b424284d
reishiki01 <reishiki01@gmail.com>
2018-08-21 21:04:10 +0800

    Base-updates Update look of index

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -10,24 +10,20 @@ class Home extends React.Component {
 	render() {
 		return (
 			<div>
-				<header className="App-header">
-	        <h1 className="App-title">Welcome to React</h1>
-	      </header>
-	      <p className="App-intro">
-	        To get started, edit <code>src/App.js</code> and save to reload.
-	      </p>
-	      <Brownies numberOfBrownies={2}/>
-	      <Button>
-	      	A Button
-	      </Button>
-	      <Button primary>
-	      	A Primary Button
-	      </Button>
-	      <Button secondary>
-	      	A Secondary Button
-	      </Button>
+				<div>
+					Navbar here
+				</div>
         <Panel>
-        	asdfjlaksdjf
+	      	<Brownies numberOfBrownies={2}/>
+		      <Button>
+		      	A Button
+		      </Button>
+		      <Button primary>
+		      	A Primary Button
+		      </Button>
+		      <Button secondary>
+		      	A Secondary Button
+		      </Button>
         </Panel>
 	    </div>
 		)
