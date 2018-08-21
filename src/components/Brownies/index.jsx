188882fc97ed719fe2bf65ed66bac866b1343c3a
stephmajarocon <samajarocon@up.edu.ph>
2018-08-21 21:00:38 +0800

    heading

--- src/components/Brownies/index.jsx
+++ src/components/Brownies/index.jsx
@@ -19,9 +19,64 @@ class Brownies extends React.Component {
 					<p>No more brownies</p>
 				}
 
+				<h1>Brownies</h1>
+				<h2>Brownies</h2>
+				<h3>Brownies</h3>
+				<h4>Brownies</h4>
+				<h5>Brownies</h5>
+
 			</div>
 		)
 	}
 }
 
 export default Brownies
+
+function Heading(props) {
+	const {level, children} = props
+
+	const getHeading = () => {
+		switch (this.props) {
+			case 1: 
+			return (
+				<h1>
+					{children}
+				</h1>
+			)
+			case 2: 
+			return (
+				<h2>
+					{children}
+				</h2>
+			)
+			case 3: 
+			return (
+				<h3>
+					{children}
+				</h3>
+			)
+			case 4: 
+			return (
+				<h4>
+					{children}
+				</h4>
+			)
+			case 5: 
+			return (
+				<h5>
+					{children}
+				</h5>
+			)
+
+		}
+	}
+}
+
+Heading.propTypes = {
+	level: PropTypes.number,
+	children: PropTypes.any.isRequired
+}
+
+Heading.defaultProps = {
+	level: 5
+}
