17bfa4c945d58551ebeae3e0ab7c5957c431e137
reishiki01 <reishiki01@gmail.com>
2018-08-21 21:01:03 +0800

    Base-updates Fix conflicts

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -11,26 +11,25 @@ class Home extends React.Component {
 		return (
 			<div>
 				<header className="App-header">
-		          <h1 className="App-title">Welcome to React</h1>
-		        </header>
-		        <p className="App-intro">
-		          To get started, edit <code>src/App.js</code> and save to reload.
-		        </p>
-		        <Brownies numberOfBrownies={2}/>
-
-		        <Button>
-		        	A Button
-		        </Button>
-		        <Button primary>
-		        	A Primary Button
-		        </Button>
-		        <Button secondary>
-		        	A Secondary Button
-		        </Button>
-		        <Panel>
-		        	asdfjlaksdjf
-		        </Panel>
-	        </div>
+	        <h1 className="App-title">Welcome to React</h1>
+	      </header>
+	      <p className="App-intro">
+	        To get started, edit <code>src/App.js</code> and save to reload.
+	      </p>
+	      <Brownies numberOfBrownies={2}/>
+	      <Button>
+	      	A Button
+	      </Button>
+	      <Button primary>
+	      	A Primary Button
+	      </Button>
+	      <Button secondary>
+	      	A Secondary Button
+	      </Button>
+        <Panel>
+        	asdfjlaksdjf
+        </Panel>
+	    </div>
 		)
 	}
 }
